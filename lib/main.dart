6659c726081f7613ee3d6ae2c273bddffaae4c53
arcanjo96 <lucasarcanjo.ld@gmail.com>
2019-07-16 15:30:52 -0300

    add new page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:buscador_gif/screens/home_page.dart';
+import 'package:buscador_gif/screens/gif_page.dart';
 
 void main() {
   runApp(MaterialApp(
